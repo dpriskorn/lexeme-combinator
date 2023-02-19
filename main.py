f1524b34887fd11e3c1f5ddb316d5c6c48f33fe2
Dennis Priskorn <egils@riseup.net>
2023-02-19 15:00:00 +0100

    fix: Update introduction.

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ logging.basicConfig(level=myconfig.loglevel)
 logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
-    console.print("Note: currently we only support lexemes "
-                  "combined by exactly two other lexemes. "
-                  "We don't currently display senses, so don't match "
-                  "if you are not sure based on the presented information."
-                  "We don't support creating new lexemes so "
-                  f"if you are missing a lexeme, go to {myconfig.ordia_url}.")
+    console.print(
+        "Note: currently we only support lexemes "
+        "combined by exactly two other lexemes. "
+        "We don't support creating new lexemes so "
+        f"if you are missing a lexeme, go to {myconfig.ordia_url}."
+    )
     comb = Combinator(lang=myconfig.language_code)
     comb.start()
     # # fetch lexemes without combines
