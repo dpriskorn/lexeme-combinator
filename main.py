4baced635d99db043ee592156a79decd15e7cdb8
Dennis Priskorn <egils@riseup.net>
2023-02-21 14:08:31 +0100

    feat: Support and make it easy for the user to specify the length of lemma to work on in config.py. Also query the maximum number of lexemes to work on and display it. Also make sure the offset is within the range 0-number of lexemes found minus 10.

--- main.py
+++ main.py
@@ -13,9 +13,11 @@ logger = logging.getLogger(__name__)
 if __name__ == "__main__":
     console.print(
         "Note: currently we only support lexemes "
-        "combined by exactly two other lexemes. "
+        "combined by exactly two other lexemes.\n"
         "We don't support creating new lexemes so "
-        f"if you are missing a lexeme, go to {myconfig.ordia_url}."
+        f"if you are missing a lexeme, go to {myconfig.ordia_url}.\n"
+        f"We look for lexemes with lemmas that contain "
+        f"a minimum of {myconfig.minimum_lemma_characters} characters"
     )
     comb = Combinator(lang=myconfig.language_code)
     comb.start()
--- src/models/combinator.py
+++ src/models/combinator.py
@@ -22,6 +22,19 @@ class Combinator(BaseModel):
     # lexical_category: str = ""
     lexemes_without_combines: List[LexemeMissingCombines] = []
     sparql_result: Dict[str, Any] = {}
+    total_number_of_lexemes: int = 0
+    total_number_sparql_result: Dict[str, Any] = {}
+    query_total_number_of_lexemes = f"""
+                        SELECT (count(?lid) as ?count) 
+                        WHERE {{
+                          ?lid dct:language wd:{config.language_qid};
+                             wikibase:lemma ?lemma.
+                          # hardcode finding lexemes with long lemmas for now
+                          filter(strlen(?lemma) >= {config.minimum_lemma_characters}) 
+                          minus {{?lid wdt:P5238 [].}} # combines
+                          minus {{?lid wdt:P5191 [].}} # derives from
+                    }}
+                    """
 
     class Config:
         arbitrary_types_allowed = True
@@ -31,6 +44,8 @@ class Combinator(BaseModel):
         """Helper method"""
         logger.debug("start: running")
         with console.status("Fetching lexemes to work on"):
+            self.__fetch_total_number_of_lexemes_of_this_length_without_combines__()
+            self.__get_total_number_from_sparql_result__()
             self.__fetch_lexemes_without_combines__()
             self.__parse_sparql_result_into_lexemes__()
         self.__iterate_lexemes__()
@@ -38,7 +53,7 @@ class Combinator(BaseModel):
     def __fetch_lexemes_without_combines__(self):
         if not config.language_code:
             raise MissingInformationError()
-        random_offset = random.randint(0, 1000)
+        random_offset = random.randint(0, self.total_number_of_lexemes - 10)
         logger.info(f"Random offset: {random_offset}")
         query_no_combines_no_derives_from = f"""
             SELECT ?lid #?lemma 
@@ -46,7 +61,7 @@ class Combinator(BaseModel):
               ?lid dct:language wd:{config.language_qid};
                  wikibase:lemma ?lemma.
               # hardcode finding lexemes with long lemmas for now
-              filter(strlen(?lemma) >= 10) 
+              filter(strlen(?lemma) >= {config.minimum_lemma_characters}) 
               minus {{?lid wdt:P5238 [].}} # combines
               minus {{?lid wdt:P5191 [].}} # derives from
         }}
@@ -80,3 +95,14 @@ class Combinator(BaseModel):
                                               f"lexeme {lexeme.lexeme_uri}")
             console.print(f"Working on {lexeme.localized_lemma}")
             lexeme.find_first_partword()
+
+    def __fetch_total_number_of_lexemes_of_this_length_without_combines__(self):
+        self.total_number_of_lexemes = 0
+        self.total_number_sparql_result = execute_sparql_query(self.query_total_number_of_lexemes)
+
+    def __get_total_number_from_sparql_result__(self):
+        if not self.total_number_sparql_result:
+            raise MissingInformationError()
+        # console.print(self.total_number_sparql_result)
+        self.total_number_of_lexemes = int(self.total_number_sparql_result["results"]["bindings"][0]["count"]["value"])
+        console.print(f"Found a total of {self.total_number_of_lexemes} lexemes")
